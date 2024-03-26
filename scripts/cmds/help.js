a2c258aeac8d43b1a4b283b58c296679a93cbfa5
Métöushela walker <160696674+Metoushael17@users.noreply.github.com>
2024-03-26 23:18:50 +0200

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ Mocha | 🧋✨ ]";
+const doNotDelete = "[ 𝙈𝙀𝙂𝘼𝙉-𝘼𝙀 | 🧋✨ ]";
 /** 
 * @author NTKhang
 * @author: do not delete it
