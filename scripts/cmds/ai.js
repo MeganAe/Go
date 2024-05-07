cc8dca869f8a5fd8fe044bbe0a6bdcbd5d9ac9fd
MeganAe <168326500+MeganAe@users.noreply.github.com>
2024-05-07 23:51:06 +0200

    Update ai.js

--- scripts/cmds/ai.js
+++ scripts/cmds/ai.js
@@ -1,61 +1,46 @@
 const axios = require('axios');
 
-const GPT_API_URL = 'https://sandipapi.onrender.com/gpt';
-const PREFIXES = ['ai'];
-const horizontalLine = "━━━━━━━━━━━━━━━";
+const Prefixes = [
+  'AI',
+  'ai',
+  'megan',
+  'Ai',
+];
 
 module.exports = {
   config: {
     name: "ai",
     version: 1.0,
-    author: "OtinXSandip",
-    longDescription: "AI",
+    author: "fatkey",
+    longDescription: "chatGptAI",
     category: "ai",
     guide: {
       en: "{p} questions",
     },
   },
-  onStart: async function () {
-    // Initialization logic if needed
-  },
+  onStart: async function () {},
   onChat: async function ({ api, event, args, message }) {
     try {
-      const prefix = PREFIXES.find((p) => event.body && event.body.toLowerCase().startsWith(p));
-
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
       if (!prefix) {
         return; // Invalid prefix, ignore the command
       }
-
       const prompt = event.body.substring(prefix.length).trim();
-
-      if (!prompt) {
-        const defaultMessage = getCenteredHeader(""📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧 | 🧋✨") + "\n" + horizontalLine + "\nHello! How can I assist you today.⚪\n" + horizontalLine;
-        await message.reply(defaultMessage);
+   if (!prompt) {
+        await message.reply("📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧\n━━━━━━━━━━━━━━\n\nHello! How can I assist you today.⚪");
         return;
       }
 
-      const answer = await getGPTResponse(prompt);
 
-      // Adding header and horizontal lines to the answer
-      const answerWithHeader = getCenteredHeader(""📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧 | 🧋✨") + "\n" + horizontalLine + "\n" + answer + "\n" + horizontalLine;
-      
-      await message.reply(answerWithHeader);
+      const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=hello${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply(`📝 𝗪𝗮𝗹𝗸𝗲𝗿𝗚𝗣𝗧:\n━━━━━━━━━━━━━━\n${answer}\n━━━━━━━━━━━━━━\n🟢 𝘔𝘦𝘵𝘰𝘶𝘴𝘩𝘦𝘭𝘢 𝘸𝘢𝘭𝘬𝘦𝘳 ⚪`);
+
     } catch (error) {
       console.error("Error:", error.message);
-      // Additional error handling if needed
     }
   }
-};
-
-function getCenteredHeader(header) {
-  const totalWidth = 32; // Adjust the total width as needed
-  const padding = Math.max(0, Math.floor((totalWidth - header.length) / 2));
-  return " ".repeat(padding) + header;
-}
-
-async function getGPTResponse(prompt) {
-  // Implement caching logic here
-
-  const response = await axios.get(`${GPT_API_URL}?prompt=${encodeURIComponent(prompt)}`);
-  return response.data.answer;
-}
+};￼Enter
